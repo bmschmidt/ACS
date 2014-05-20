4a73f8b5b75a0b45be143bddb5782e2327b22764
Benjamin Schmidt <bmschmidt@gmail.com>
2014-05-20 17:48:32 -0400

    porting to server

--- Makefile
+++ Makefile
@@ -1,8 +1,14 @@
 
 
-database:
+database: csv_pus/ss12pusa.csv
 	mysql < makeDatabase.SQL
-	mysql -e "LOAD DATA LOCAL INFILE '$filename' INTO TABLE persons FIELDS TERMINATED BY ',' IGNORE 1 LINES" ACS
+	touch database
+
+csv_pus.zip:
+	wget http://www2.census.gov/acs2012_3yr/pums/csv_pus.zip
+
+csv_pus/ss12pusa.csv: csv_pus.zip
+	unzip csv_pus.zip
 
 Sequence_Number_and_Table_Number_Lookup.txt:
 	wget http://ftp2.census.gov/acs2012_3yr/summaryfile/Sequence_Number_and_Table_Number_Lookup.txt
@@ -10,7 +16,9 @@ Sequence_Number_and_Table_Number_Lookup.txt:
 codebook.txt: PUMS_Data_Dictionary_2010-2012.pdf
 	pdf2txt.py PUMS_Data_Dictionary_2010-2012.pdf > codebook.txt
 
+weights.txt: database
+	mysql -e "SELECT SERIALNO, PWGTP1,PWGTP2,PWGTP3,PWGTP4,PWGTP5,PWGTP6,PWGTP7,PWGTP8,PWGTP9,PWGTP10,PWGTP11,PWGTP12,PWGTP13,PWGTP14,PWGTP15,PWGTP16,PWGTP17,PWGTP18,PWGTP19,PWGTP20,PWGTP21,PWGTP22,PWGTP23,PWGTP24,PWGTP25,PWGTP26,PWGTP27,PWGTP28,PWGTP29,PWGTP30,PWGTP31,PWGTP32,PWGTP33,PWGTP34,PWGTP35,PWGTP36,PWGTP37,PWGTP38,PWGTP39,PWGTP40,PWGTP41,PWGTP42,PWGTP43,PWGTP44,PWGTP45,PWGTP46,PWGTP47,PWGTP48,PWGTP49,PWGTP50,PWGTP51,PWGTP52,PWGTP53,PWGTP54,PWGTP55,PWGTP56,PWGTP57,PWGTP58,PWGTP59,PWGTP60,PWGTP61,PWGTP62,PWGTP63,PWGTP64,PWGTP65,PWGTP66,PWGTP67,PWGTP68,PWGTP69,PWGTP70,PWGTP71,PWGTP72,PWGTP73,PWGTP74,PWGTP75,PWGTP76,PWGTP77,PWGTP78,PWGTP79,PWGTP80 FROM persons" ACS | awk 'BEGIN{FS="\t";OFS="\t"}{ if ($$1!="SERIALNO") {for(i = 2; i <= NF; i++) { print $$1,i-1,$$i; }}}' > weights.txt
 
 codebook.tsv: codebook.txt
 #There are some weird whitespace characters in here...
-	perl -ne 'if (m/^([A-Z0-9]{1,}) +[0-9]/){$$field=$$1};if (m/^\s{4,5}([A-Z].*)/){$$realname=$$1};if (m/^ {10,}([0-9]+) \.(.*)/) {$$code=$$1;$$name=$$2; print "$$field\t$$realname\t$$code\t$$name\n"}' $< > $@
+	perl -ne 's/\s/ /g; if (m/^([A-Z0-9]{1,}) +[0-9]/){$$field=$$1};if (m/^\s{4,5}([A-Z].*[^ ])/){$$realname=$$1};if (m/^ {10,}([0-9]+) \.(.*[^ ])/) {$$code=$$1;$$name=$$2; print "$$field\t$$realname\t$$code\t$$name\n"}' $< > $@
--- functions.R
+++ functions.R
@@ -1,13 +1,143 @@
+library(dplyr)
+library(ggplot2)
+
+returnWeights = function(weights=1:80) {
+  if (!file.exists("weights.RData")) {
+    persons = tbl(src=tables,"persons")
+    query = 
+      paste("SELECT SERIALNO,",paste0("PWGTP",1:80,collapse=","), "FROM persons LIMIT 30")
+    elements = list(.data=persons,list(c("SERIALNO",paste0("PWGTP",weights))))
+    data = do.call(s_select,elements) %.% collect()
+    library(reshape2)
+    n = melt(data,id.vars="SERIALNO")
+    names(n) = c("SERIALNO","sample","weight")
+    #I don't care if the sample numbers are the same, so just coerce
+    n$sample = as.numeric(n$sample)
+    save(n,file='weights.RData')
+    return(n)
+  } else {
+    rm(n)
+    load("weights.RData")
+    return(n)
+  }
+}
+
+writeOutFields = function() {
+  #
+  counts = persons %.% group_by(FOD1P) %.% summarize(count=n()) %.% arrange(-count) %.% collect()
+  named = counts %.% encode("FOD1P")
+  head(named)  
+  write.table(named,file="majorNames.tsv",sep="\t",quote=F,row.names=F)
+  #Then I assigned some values at MajorFields.tsv
+}
 
 encode = function(tab,fieldsname="FOD1P",looker=codebook) {
-  lookups = looker %.% filter(field==fieldsname) %.% mutate(value = gsub(" $","",value),code=as.integer(code))
+  #gives a factor sensible names from a codebook 
+  lookups = looker %.% 
+    filter(field==fieldsname) %.% 
+    mutate(value = gsub(" $","",value),code=as.integer(code))
+  counts = table(tab[,fieldsname])
+  missingCounts = counts[!names(counts) %in% lookups$code]
   tab[,fieldsname] = 
     factor(tab[,fieldsname],levels = lookups$code,labels=lookups$value)
   tab
 }
 
-filterToTopN = function(frame,variable,n=30) {
-  topN = frame %.% regroup(list(variable)) %.% summarise(groupingCount=sum(PWGTP)) %.% top_n(n)
+filterToTopN = function(frame,variable,n=30,countVariable=quote(PWGTP)) {
+  topN = frame %.% regroup(list(variable)) %.% summarise(groupingCount=sum(countVariable)) %.% top_n(n)
   topN = topN[,1,drop=F]
   frame %.% inner_join(topN) 
 }
+
+
+factorByPrincomps = function(frame) {
+head(frame)
+
+}
+
+
+
+
+
+
+
+#dplyr string workarounds:
+# Helper functions that allow string arguments for  dplyr's data modification functions like arrange, select etc. 
+# Author: Sebastian Kranz
+
+# Examples are below
+
+#' Modified version of dplyr's filter that uses string arguments
+#' @export
+s_filter = function(.data, ...) {
+  eval.string.dplyr(.data,"filter", ...)
+}
+
+#' Modified version of dplyr's select that uses string arguments
+#' @export
+s_select = function(.data, ...) {
+  eval.string.dplyr(.data,"select", ...)
+}
+
+#' Modified version of dplyr's arrange that uses string arguments
+#' @export
+s_arrange = function(.data, ...) {
+  eval.string.dplyr(.data,"arrange", ...)
+}
+
+#' Modified version of dplyr's arrange that uses string arguments
+#' @export
+s_mutate = function(.data, ...) {
+  eval.string.dplyr(.data,"mutate", ...)
+}
+
+#' Modified version of dplyr's summarise that uses string arguments
+#' @export
+s_summarise = function(.data, ...) {
+  eval.string.dplyr(.data,"summarise", ...)
+}
+
+#' Modified version of dplyr's group_by that uses string arguments
+#' @export
+s_group_by = function(.data, ...) {
+  eval.string.dplyr(.data,"group_by", ...)
+}
+
+#' Internal function used by s_filter, s_select etc.
+eval.string.dplyr = function(.data, .fun.name, ...) {
+  args = list(...)
+  args = unlist(args)
+  code = paste0(.fun.name,"(.data,", paste0(args, collapse=","), ")")
+  df = eval(parse(text=code,srcfile=NULL))
+  df  
+}
+
+# Examples
+library(dplyr)
+
+# Original usage of dplyr
+mtcars %.%
+  filter(gear == 3,cyl == 8) %.%  
+  select(mpg, cyl, hp:vs)
+
+# Select user specified cols.
+# Note that you can have a vector of strings
+# or a single string separated by ',' or a mixture of both
+cols = c("mpg","cyl, hp:vs")
+mtcars %.%
+  filter(gear == 3,cyl == 8) %.%  
+  s_select(cols)
+
+# Filter using a string
+col = "gear"
+mtcars %.%
+  s_filter(paste0(col,"==3"), "cyl==8" ) %.%
+  select(mpg, cyl, hp:vs)
+
+# Arrange without using %.%
+s_arrange(mtcars, "-mpg, gear, carb")
+
+# group_by and summarise with strings
+mtcars %.%
+  s_group_by("cyl") %.%  
+  s_summarise("mean(disp), max(disp)")
